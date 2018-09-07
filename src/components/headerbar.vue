a203f85304bfb6f63f37061893bbd4f79fe036b2
Jaime Pastor <jaime.pastor@adidas-group.com>
2018-09-07 13:26:06 +0200

    Fix home page style

--- src/components/headerbar.vue
+++ src/components/headerbar.vue
@@ -1,20 +1,17 @@
 <template>
   <header class="header">
     <nav class="header__container">
-      <div class="header__main">
-        <div class="header__left">
-          <nuxt-link :to="localePath('index')"
-              class="header__brand link"
-              exact-active-class="">
-            <span class="header__logo yarn-logo adidas"></span>
-            <span class="header__headline">{{ $t('meta.title') }}</span>
-          </nuxt-link>
-        </div>
-        <div class="header__right">
-          <nuxt-link :to="localePath('contributing')" class="btn btn--blue">
-            {{ $t('links.contributing') }}
-          </nuxt-link>
-        </div>
+      <div class="header__left">
+        <nuxt-link :to="localePath('index')"
+            class="header__brand">
+          <span class="header__icon yarn-logo adidas"></span>
+          <span class="header__headline">{{ $t('meta.title') }}</span>
+        </nuxt-link>
+      </div>
+      <div class="header__right">
+        <nuxt-link :to="localePath('contributing')" class="btn btn--blue">
+          {{ $t('links.contributing') }}
+        </nuxt-link>
       </div>
     </nav>
   </header>
