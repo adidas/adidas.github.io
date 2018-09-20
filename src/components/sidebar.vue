d8e7d7d35767cafad55cd5d25c7fc5c63e0b571b
Jaime Pastor <jaime.pastor@adidas-group.com>
2018-09-20 12:31:39 +0200

    Fix styles

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -1,15 +1,8 @@
 <template>
   <aside class="sidebar">
-    <div class="sidebar__block">
-      <header class="sidebar__header">
-        <span class="sidebar__title label">
-          {{ $t('views.contributing.sections') }}
-        </span>
-      </header>
-      <div class="sidebar__main">
-        <div class="list-group list-group--vertical">
-          <sidebar-list v-for="(item, index) in tree" :key="index" :model="item"/>
-        </div>
+    <div class="sidebar__main">
+      <div class="list-group list-group--vertical">
+        <sidebar-list v-for="(item, index) in tree" :key="index" :model="item"/>
       </div>
     </div>
   </aside>
@@ -29,27 +22,9 @@ export default {
 </script>
 
 <style lang="scss">
-  @import '../styles/vars';
-
   .sidebar {
     &__main {
-      .list-group {
-        font-size: $font-size-normal;
-        padding-top: 0 !important;
-      }
-
-      .list-group-item {
-        padding-bottom: 0 !important;
-      }
-
-      > .list-group {
-        font-size: $font-size-small;
-
-        > .list-group-item > a,
-        > .list-group-item > span{
-          font-weight: bold;
-        }
-      }
+      position: fixed;
     }
   }
 </style>
