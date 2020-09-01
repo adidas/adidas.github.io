250ec9939285e7f2ad28eca6294752d5778109cb
David Recuenco <RecuencoJones@users.noreply.github.com>
2020-09-01 15:16:51 +0200

    USE intl numberformat (#25)
    * USE intl numberformat
    * USE navigator language
    * REMOVE redundant test

--- src/utils/format-number.js
+++ src/utils/format-number.js
@@ -1,21 +1,3 @@
-const GROUP_LENGTH = 3;
-const NUMBER_DECIMAL_SEPARATOR = '.';
-const separatorDefaults = {
-  groupSeparator: '.',
-  decimalSeparator: ','
-};
-
-export function formatNumber(value, { groupSeparator, decimalSeparator } = separatorDefaults) {
-  const [ integer, decimals ] = `${ value }`.split(NUMBER_DECIMAL_SEPARATOR);
-  const chars = `${ integer }`.split('').reverse();
-  const parts = Math.ceil(chars.length / GROUP_LENGTH);
-  const groups = [];
-
-  for (let i = 0; i < parts; i++) {
-    groups.push(chars.slice(i * GROUP_LENGTH, (i + 1) * GROUP_LENGTH).reverse().join(''));
-  }
-
-  const groupedInteger = groups.reverse().join(groupSeparator);
-
-  return decimals ? [ groupedInteger, decimals ].join(decimalSeparator) : groupedInteger;
+export function formatNumber(value) {
+  return new Intl.NumberFormat(navigator.language).format(value);
 }
